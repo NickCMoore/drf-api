1b9e55ed0e05396d33f85da000599bad51c6e307
Nick Moore <moorenickc@protonmail.com>
2024-09-23 22:56:58 +0000

    update secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -25,7 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-os.environ.setdefault("SECRET_KEY", "0jV:8dk>w[V]E*:9bt8e!$u[mEt6SrT(Wj*%d7qrGw$^:J3@$;5t(.TaYzC(wW>.")
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
