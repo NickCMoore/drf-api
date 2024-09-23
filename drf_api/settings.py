17fc45288e5033138342f72b45dc7e7f45d8066c
Nick Moore <moorenickc@protonmail.com>
2024-09-23 11:35:19 +0000

    remove static files directory

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -120,17 +120,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
 
-# Define additional static files directories if necessary
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Define the location for static files collection
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
